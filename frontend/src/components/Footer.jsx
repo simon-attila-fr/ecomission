9623f5ac9a7440a1be202ab5444e2f9413a9d16e
Attila Simon <simon.attila.fr@gmail.com>
2022-05-12 16:34:44 +0200

    Add Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,21 @@
 import "../App.css";
 import "./styles/Footer.css";
+import fb from "../assets/fb.png";
+import insta from "../assets/insta.png";
+import twitter from "../assets/twitter.png";
 
 export default function Footer() {
-  return <div />;
+  return (
+    <>
+      <div className="footerSocialMedia">
+        <img src={fb} alt="Logo de Facebook" />
+        <img src={insta} alt="Logo d'Instagram" />
+        <img src={twitter} alt="Logo of Twitter" />
+      </div>
+      <div className="footerText">
+        <p>© Capitains de Planète</p>
+        <p>contact@capitainsdeplanete.com</p>
+      </div>
+    </>
+  );
 }
