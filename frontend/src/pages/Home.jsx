f7434e99fa890ec22d1af2f899c92026e06fcd76
rodrigue <rodrigue_techer@yahoo.com>
2022-05-12 19:28:18 +0200

    add context

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,21 @@ export default function Home() {
   return (
     <div>
       <NavBar home="Acceuil" infos="Infos" />
-      <h1>Home</h1>
+      <div className="home">
+        <h1 className="slogan">
+          Limter les émissions est une mission possible
+        </h1>
+        <p>
+          Why do we use it? It is a long established fact that a reader will be
+          distracted by the readable content of a page when looking at its
+          layout. The point of using Lorem Ipsum is that it has a more-or-less
+          normal distribution of letters, as opposed to using Content here,
+          content here, making it look like readable English. Many desktop
+          publishing packages and web page editors now use Lorem Ipsum as their
+          default model text
+        </p>
+        <button className="recherchemodel">rechrcher</button>
+      </div>
     </div>
   );
 }
