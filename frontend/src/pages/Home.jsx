be5c6326bb643e8ca61711e2f3ec8357f944d064
KevinFampou <fampoukevin3@gmail.com>
2022-05-13 11:56:47 +0200

    some css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -38,11 +38,7 @@ export default function Home() {
             <p>
               Why do we use it? It is a long established fact that a reader will
               be distracted by the readable content of a page when looking at
-              its layout. The point of using Lorem Ipsum is that it has a
-              more-or-less normal distribution of letters, as opposed to using
-              Content here, content here, making it look like readable English.
-              Many desktop publishing packages and web page editors now use
-              Lorem Ipsum as their default model text
+              its layout.
             </p>
           </div>
           <div className="select">
@@ -51,7 +47,7 @@ export default function Home() {
               name=""
               id=""
             >
-              <option value="">---</option>
+              <option value="">MARQUES</option>
               {marques.map((marque) => (
                 <option>{marque}</option>
               ))}
@@ -61,7 +57,7 @@ export default function Home() {
               name=""
               id=""
             >
-              <option value="">---</option>
+              <option value="">MODELES</option>
               {ObjectMarques.map((modele) => (
                 <option>
                   {modele.fields.hybride === undefined ||
